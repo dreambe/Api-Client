59373136394b77b645aed40b2489a83d70ad71d4
wangshaokai <shaoki.wang@gmail.com>
2017-08-11 20:17:01 +0800

    del junk code

--- client.py
+++ client.py
@@ -1,6 +1,5 @@
 from tkinter import *
 import threading
-import json
 
 import utils
 import view
@@ -8,21 +7,12 @@ import httpclient
 
 
 class Client(view.ClientView):
-    """docstring for  Client"view.ClientView def __init__(self, arg):
-        super( Client,view.ClientView.__init__()
-        self.arg = arg
-    """
-
     def __init__(self, master=None):
         super(Client, self).__init__()
         self.root = master
         self.test = httpclient.HttpSession()
 
     def submit(self):
-        """
-        点击提交按钮创建新线程
-        :return:None
-        """
         # 禁用提交按钮
         self._send_button.configure(state="disabled", text="提交中")
         # 启动线程
@@ -31,12 +21,7 @@ class Client(view.ClientView):
         t.start()
 
     def submit_event(self):
-        """
-        提交请求
-        :return:None
-        """
         url = self._url_entry.get()
-        # 地址开头没有http:自动加上http://
         if url.find("http"):
             self._url_entry.insert(0, "http://")
             url = "http://" + url
@@ -52,19 +37,12 @@ class Client(view.ClientView):
         print('response_headers \n', response_meta['response_headers'])
         print('response_content \n', response_meta['response_content'])
 
-        # response_meta['response_content']['errmsg'] += "\n\n请求时间：" + str(response_meta['response_time']) + "s"
-        # result_data = json.loads(response_meta['response_content'].decode('utf-8'))
         result_data = utils.assemble(request_meta, response_meta)
         self.result(result_data)
         # 启用提交按钮
         self._send_button.configure(state="normal", text="提交")
 
     def result(self, result_data):
-        """
-        写入结果到UI
-        :param result_data: 需要写入的信息
-        :return:None
-        """
         print(result_data)
         self.clear_text()
         if result_data['errno'] == 0:
@@ -95,9 +73,7 @@ class Client(view.ClientView):
     def insert_text(self, result_data):
 
         self._header_text.insert(END, result_data['request'])
-        # self._header_text.insert(END, result_data['msg'] + "\n")
         self._header_text.insert(END, result_data['errmsg'] + "\n")
-        # self._body_text.insert(END, result_data['read'])
         self._body_text.insert(END, result_data['rep_body'])
 
 
